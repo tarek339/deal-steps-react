7a6a78c3ac8b6ef28f8dd2078a7367187910880c
Tarek Jassine <tarekjassine@gmail.com>
2024-08-28 13:59:59 +0200

    optimized comp and UI

--- src/components/CartTable.tsx
+++ src/components/CartTable.tsx
@@ -1,41 +1,57 @@
 import { ICartTable } from "../types/interfaces/components";
-import ContentHolder from "./ContentHolder";
+import Button from "./Button";
+import Flex from "./Flex";
 
 const CartTable = ({ children, priceData }: ICartTable) => {
   return (
-    <ContentHolder mt={24}>
-      <div className="flex flex-col mx-5">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden shadow-md">
-              <table className="min-w-full text-left text-sm font-light">
-                <thead className="border-b border-[#262626] bg-[#d5d5d5] font-medium dark:border-neutral-500 dark:bg-neutral-600">
-                  <tr>
-                    <th scope="col" className="px-6 py-4">
-                      #
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Brand
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Quantity
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Price
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Delete
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>{children}</tbody>
-              </table>
-            </div>
+    <div className="flex flex-col mx-5">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div className="overflow-hidden shadow-md">
+            <table className="min-w-full text-left text-sm font-light">
+              <thead className="border-b border-[#262626] bg-[#d5d5d5] font-medium dark:border-neutral-500 dark:bg-neutral-600">
+                <tr>
+                  <th scope="col" className="px-6 py-4">
+                    #
+                  </th>
+                  <th scope="col" className="px-6 py-4">
+                    Brand
+                  </th>
+                  <th scope="col" className="px-6 py-4">
+                    Quantity
+                  </th>
+                  <th scope="col" className="px-6 py-4">
+                    Price
+                  </th>
+                  <th scope="col" className="px-6 py-4">
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>{children}</tbody>
+            </table>
           </div>
         </div>
-        {priceData}
       </div>
-    </ContentHolder>
+      <Flex col gap={3} mt={3}>
+        <Flex end wFull>
+          <h3 className="mb-2.5 text-3xl font-medium leading-tight">
+            Total Price:{" "}
+            <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-primary-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-success-700">
+              {priceData} €
+            </span>
+          </h3>
+        </Flex>
+        <Flex end wFull>
+          <Button
+            title={"procceed tp pay"}
+            type={undefined}
+            rippleColor={"light"}
+            bgColor={"primary"}
+          />
+        </Flex>
+      </Flex>
+    </div>
   );
 };
 
--- src/views/product/ProductCart.tsx
+++ src/views/product/ProductCart.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import withRestrictions from "../../hoc/withRestrictions";
 import { useEffect, useState } from "react";
-import { Button, CartTable, Flex } from "../../components";
+import { CartTable, ContentHolder, SubTitle } from "../../components";
 import { useSelectors } from "../../hooks";
 import { ICart } from "../../types/interfaces/interfaces";
 import IconButton from "../../components/IconButton";
@@ -44,55 +44,42 @@ const ProductCart = () => {
   }, []);
 
   return (
-    <div>
-      <CartTable
-        children={cart.map((item, i) => {
-          return (
-            <>
-              <tr
-                key={item.id}
-                className="border-b odd:bg-neutral-100 even:bg-white hover:bg-[#d5d5d5] transition-bg duration-300 cursor-pointer">
-                <td className="whitespace-nowrap px-6 py-4 font-medium">
-                  {i + 1}
-                </td>
-                <td className="whitespace-nowrap px-6 py-4">{item.brand}</td>
-                <td className="whitespace-nowrap px-6 py-4">{item.quantity}</td>
-                <td className="whitespace-nowrap px-6 py-4">
-                  {item.totalPrice}
-                </td>
-                <td className="whitespace-nowrap px-6 py-4">
-                  <IconButton
-                    transparent
-                    icon={<Delete color={"red"} size={25} />}
-                    onClick={() => removeFromCart(item.id)}
-                  />
-                </td>
-              </tr>
-            </>
-          );
-        })}
-        priceData={
-          <Flex col gap={3} mt={3}>
-            <Flex end wFull>
-              <h3 className="mb-2.5 text-3xl font-medium leading-tight">
-                Total Price:{" "}
-                <span className="inline-block whitespace-nowrap rounded-[0.27rem] bg-primary-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-primary-700">
-                  {total} €
-                </span>
-              </h3>
-            </Flex>
-            <Flex end wFull>
-              <Button
-                title={"procceed tp pay"}
-                type={undefined}
-                rippleColor={"light"}
-                bgColor={"primary"}
-              />
-            </Flex>
-          </Flex>
-        }
-      />
-    </div>
+    <ContentHolder mt={24}>
+      {cart.length > 0 ? (
+        <CartTable
+          children={cart.map((item, i) => {
+            return (
+              <>
+                <tr
+                  key={item.id}
+                  className="border-b odd:bg-neutral-100 even:bg-white hover:bg-[#d5d5d5] transition-bg duration-300 cursor-pointer">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">
+                    {i + 1}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{item.brand}</td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {item.quantity}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {item.totalPrice}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    <IconButton
+                      transparent
+                      icon={<Delete color={"red"} size={25} />}
+                      onClick={() => removeFromCart(item.id)}
+                    />
+                  </td>
+                </tr>
+              </>
+            );
+          })}
+          priceData={total}
+        />
+      ) : (
+        <SubTitle center title={"Your cart is empty. Go shopping..."} />
+      )}
+    </ContentHolder>
   );
 };
 
